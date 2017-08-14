5f1f9f77c903b818fb73740b5ba420794ed680d5
khaled shaaban <khaled@khaleds-MacBook-Pro.local>
2017-08-14 20:26:19 +0100

    Moved inline styles to stylesheet and created img tags instead of background image

--- src/BookItem.js
+++ src/BookItem.js
@@ -25,7 +25,7 @@ class BookItem extends Component {
           <li key={book.id}>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-cover" ><img src={book.imageLinks.thumbnail} alt={book.title}/></div>
                 <div className="book-shelf-changer">
                   <select value={book.shelf} onChange={(event) => updateShelf(book, event.target.value)}>
                     <option value="default" disabled>Move to...</option>
